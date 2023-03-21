42bb9bdba5d4e0208ca214cbd127932698877903
eunsikim <kimeunsik229@gmail.com>
2023-03-20 21:14:31 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', function(){
+    this.classList.toggle('is-active');
+    document.querySelector('.ham-menu').classList.toggle('is-active');
+});
+
+const navBar = document.querySelector('.nav-bar');
+
+const height = window.innerHeight * .75;
+
+window.onscroll = () => {
+    "use strict";
+
+    
+
+    if(document.body.scrollTop >= height || document.documentElement.scrollTop >= height){
+        navBar.classList.add("scroll");
+    }
+    else{
+        navBar.classList.remove('scroll');
+    }
+}
+
