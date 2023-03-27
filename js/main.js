8f10c84db43ef6ff36df9c8890a94e45840e9398
eunsikim <kimeunsik229@gmail.com>
2023-03-27 15:05:50 -0400

    Implemented links to secrtion and social bar hyperlinks

--- js/main.js
+++ js/main.js
@@ -22,3 +22,14 @@ window.onscroll = () => {
     }
 }
 
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
